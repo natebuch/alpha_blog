6d4bf65f42dda57b00ed67025df5d6837c2e9c77
nbuch <nbuch@nbuchs-mbp.attlocal.net>
2018-06-18 21:12:19 -0500

    Add password salting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180617181336) do
+ActiveRecord::Schema.define(version: 20180617194957) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180617181336) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
