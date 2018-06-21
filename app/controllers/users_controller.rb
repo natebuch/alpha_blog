6eb41eac8cb6bc2aa76381b7119741a33b93703d
nbuch <nbuch@nbuchs-mbp.attlocal.net>
2018-06-20 23:39:30 -0500

    Add user index for all articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+
+
   def new
     @user = User.new  
   end
@@ -25,6 +31,10 @@ class UsersController < ApplicationController
     else
       render "edit"
     end
+  end
+
+  def show
+  	@user = User.find(params[:id])
   end
 
   private
